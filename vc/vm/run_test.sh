237e0da610ad96cd729bab5263ad6860379c4051
Fernando Ipar <fipar@acm.org>
2015-11-16 11:46:54 -0300

    added test script

---
+++ vc/vm/run_test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+. worker_pool.sh
+export _worker_pool_WORKERS=10
+no_of_queries=$(ls x*|wc -l)
+
+worker()
+{
+   query_file=$(ls x*|head -$((1 + RANDOM % no_of_queries))|tail -1)
+   mysql -h 127.0.0.1 drupal < $query_file
+}
+
+while [ ! -f /tmp/stop_test ]; do
+   _worker_pool_start_worker_or_wait_for_slot worker 
+done
